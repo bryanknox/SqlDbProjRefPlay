94576d4e1a8cb4cd0cd4eb2be61699d21d814fdc
Bryan Knox <bryan.knox@xebia.com>
2025-01-30 14:39:28 -0800

    Add Database1 sqlproj and StreetAddress table

---
+++ Database1/dbo/Tables/dbo.StreetAddress.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[StreetAddress]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+    , [Street] NVARCHAR(100) NOT NULL
+    , [City] NVARCHAR(50) NOT NULL
+    , [State] CHAR(2) NOT NULL
+    , [Zip] CHAR(5) NOT NULL
+)
